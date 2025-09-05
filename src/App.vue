fcf7f83d29a37649939b27d0a670c964d05e5210
David Akunts <david@akunts.ru>
2025-09-05 11:47:50 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <InputComp />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import InputComp from "./components/InputComp.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    InputComp,
   },
 };
 </script>
